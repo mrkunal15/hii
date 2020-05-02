ef6c7a6894782818670ae327012000fb7199ae6e
mekunal15 <kunallaheri115@gmail.com>
2020-05-02 18:37:05 +0530

    Updates

---
+++ precache-manifest.a9ad540bd2dac566d01ae3fe0ff741ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7495b460a1db673099ec6dd605e82473",
+    "url": "/hii/index.html"
+  },
+  {
+    "revision": "f3193243d8a3fe95da6a",
+    "url": "/hii/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "652ae790720b6d1cbf34",
+    "url": "/hii/static/js/2.e31af3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hii/static/js/2.e31af3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3193243d8a3fe95da6a",
+    "url": "/hii/static/js/main.462ad4a4.chunk.js"
+  },
+  {
+    "revision": "280913573e22f721b2eb",
+    "url": "/hii/static/js/runtime-main.741bb66f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hii/static/media/logo.5d5d9eef.svg"
+  }
+]);
